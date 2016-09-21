b455daa2364db6448b4c11156994db15b40c6fd9
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2016-09-21 19:14:47 +0900

    Switch production/development for monaco

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,16 @@
 const defaultConfigFunc = require('@kadira/storybook/dist/server/config/defaults/webpack.config');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const environment = process.env.NODE_ENV;
+const isProduction = environment === 'production';
+
 function configure(base) {
   const config = defaultConfigFunc(base);
-  config.devtool = 'eval-source-map';
+  config.devtool = isProduction ? 'source-map' : 'eval-source-map';
   config.plugins.push(
     new CopyWebpackPlugin([
       {
-        from: 'node_modules/monaco-editor/min/vs',
+        from: `node_modules/monaco-editor/${isProduction ? 'min' : 'dev'}/vs`,
         to: 'vs',
       },
     ])
