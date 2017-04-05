a25313b78cdb8d2a47771daf2942846dbe32995e
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2017-04-05 11:36:58 +0900

    Fix scrollbar issue
    In some environment, height of header is bigger than expected

--- src/Page.jsx
+++ src/Page.jsx
@@ -7,7 +7,7 @@ import logo from '../docs/assets/cattz-10-character.png';
 export default function Page(props) {
   const pageName = props.match.params.page;
   return (<div>
-    <div>
+    <div style={{ height: 33 + 4 }}>
       <NavLink to="/"><img src={logo} alt="cattaz" width="100" height="33" /></NavLink>
       <span style={{ margin: '0 0.5em', verticalAlign: 'top', fontSize: '24px' }}>{pageName}</span>
     </div>
