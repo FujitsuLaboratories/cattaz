b8b07e3ee0b500a5fb6194982fc92d43f3637655
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2017-12-15 17:29:01 +0900

    Fix deployment error
    awscli gets upgraded from 1.13.0 to 1.14.10.
    awscli raises an error while deploying.
    
    Successfully built ecd552a38549
    Successfully tagged test:latest
    unknown shorthand flag: 'e' in -e
    See 'docker login --help'.
    
    I guess this stackoverflow answer will solve the issue.
    https://stackoverflow.com/questions/44722341/docker-login-unknown-shorthand-flag-e/44722622#44722622

--- scripts/deploy-to-aws-ecs.sh
+++ scripts/deploy-to-aws-ecs.sh
@@ -12,7 +12,7 @@ awsAccountId=$(aws sts get-caller-identity | sed s/\"/\\n/g | grep "^[0-9]*$")
 imageVersion=travis-${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT}
 imageName=${awsAccountId}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${imageVersion}
 
-$(aws ecr get-login --region $AWS_DEFAULT_REGION)
+$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
 
 docker tag test:latest "$imageName"
 docker push "$imageName"
